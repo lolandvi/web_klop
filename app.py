16db816ad4c05b2e9b5dd88c7dd812bdb89fd964
Клопенкова Виктория <lvikulka@bk.ru>
2024-01-10 02:56:03 +0700

    Статистические файлы

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
+    return redirect("/menu", code=302)
+
+
+@app.route("/menu")
+def menu():
     return '''
 <!doctype html>
 <html>
     <head>
-        <title>Клопенкова Виктория Владимировна, Лабораторная № 1</title>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
     </head>
     <body>
         <header>
-            НГТУ, ФБ, Лабораторная работа № 1
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных,
         </header>
         
         <h1>web-сервер на flask</h1>
@@ -21,4 +27,51 @@ def start():
         </footer>    
     </body>
 </html>    
+'''
+
+@app.route("/lab1")
+def lab1():
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>Клопенкова Виктория Владимировна, Лабораторная работа № 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа № 1
+        </header>
+        
+        <h1>web-сервер на flask</h1>
+
+        <main>
+            <br>
+            <div>
+                Flask — фреймворк для создания веб-приложений на языке
+                программирования Python, использующий набор инструментов
+                Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+                называемых микрофреймворков — минималистичных каркасов
+                веб-приложений, сознательно предоставляющих лишь самые ба-
+                зовые возможности.
+            </div>
+            <br>
+        </main>
+
+        <footer>
+            &copy; Клопенкова Виктория, ФБИ-11, 3 курс, 2023
+        </footer>    
+    </body>
+</html>    
+'''
+
+@app.route('/lab1/oak')
+def oak():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>Дуб</h1>
+        <img src="''' + url_for('static', filename='oak.jpg') + '''">
+    </body>
+</html>
 '''
