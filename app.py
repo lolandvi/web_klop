55c8e4b4ebf8c91fbcefaf133f6aaf10fcaf8338
Клопенкова Виктория <lvikulka@bk.ru>
2024-01-13 09:01:34 +0700

    Сохранить

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -55,6 +55,14 @@ def lab1():
                 зовые возможности.
             </div>
             <br>
+            <a href='/menu'>Назад к меню</a>
+
+            <h2>Реализованные роуты</h2>
+            <ul>
+            <li><a href='/lab1/oak'>Дуб</a></li>
+            <li><a href='/lab1/student'>Студенты</a></li>
+            <li><a href='/lab1/python'>О Python</a></li>
+            </ul>
         </main>
 
         <footer>
@@ -79,18 +87,12 @@ def oak():
 </html>
 '''
 
-@app.route('/lab1/students')
+@app.route('/lab1/student')
 def students():
     return '''
 <!doctype html>
 <html>
     <body>
-        <header>
-            НГТУ, ФБ, Лабораторная работа № 1
-        </header>
-        
-        <h1>web-сервер на flask</h1>
-
         <main>
             <br>
             <div>
@@ -99,7 +101,7 @@ def students():
             <br>
 
             <h1>Логотип</h1>
-            <img src="''' + url_for('static', filename='лого.jpg') + '''" width=450px, height=300px>
+            <img src="''' + url_for('static', filename='лого.jpg') + '''" width=500px, height=300px>
         </main>
 
         <footer>
@@ -109,3 +111,63 @@ def students():
 </html>    
 '''
 
+@app.route('/lab1/python')
+def python():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <main>
+            <br>
+            <div>
+                Python — это интерпретируемый, объектно-ориентированный язык программирования, который 
+                известен своей чистотой и простотой в использовании. Он предоставляет широкие возможности 
+                разработчикам благодаря своей читаемости и простоте синтаксиса. Python активно используется 
+                во многих сферах, начиная от веб-разработки и научных исследований и заканчивая созданием игр, 
+                анализом данных и разработкой искусственного интеллекта. Его гибкость и многофункциональность 
+                делают его одним из самых популярных языков программирования в мире.
+            </div>
+            <br>
+            <div>
+                Одним из фундаментальных преимуществ Python является его богатая экосистема библиотек. 
+                Библиотеки, такие как NumPy, Pandas, Matplotlib, TensorFlow и многие другие, делают Python 
+                основным инструментом для научных исследований, обработки данных и машинного обучения. Кроме 
+                того, наличие фреймворков веб-разработки, таких как Django и Flask, делают Python эффективным 
+                выбором для создания веб-приложений и API. Большое количество библиотек делает Python незаменимым 
+                инструментом для широкого круга разработок, включая робототехнику, игровую индустрию, научные 
+                исследования и многое другое.
+            </div>
+            <br>
+            <div>
+                Python продолжает оставаться актуальным благодаря своей дружелюбной и поддерживающей разработчиков 
+                атмосфере. Активное сообщество разработчиков постоянно обогащает его экосистему, добавляя новые 
+                библиотеки и инструменты, что поддерживает его рост и популярность в современной разработке программного обеспечения.
+            </div>
+            <br>
+            <img src="''' + url_for('static', filename='питон.jpg') + '''" width=500px, height=350px>
+        </main>
+
+        <footer>
+            &copy; Клопенкова Виктория, ФБИ-11, 3 курс, 2023
+        </footer>    
+    </body>
+</html>    
+'''
+
+@app.route('/proba', methods=['GET', 'POST'])
+def proba():
+    result = None
+    if request.method == 'POST':
+        first_value = request.form['first_value']
+        second_value = request.form['second_value']
+        if first_value and second_value:
+            if first_value.isdigit():  # Проверяем, что первое значение является числом
+                result = ' '.join([second_value for _ in range(int(first_value))])
+            else:
+                result = 'Первое значение должно быть числом!'
+        else:
+            result = 'Оба поля должны быть заполнены!'
+    return render_template('input.html', result=result)
+
+if __name__ == '__main__':
+    app.run()
